f7f8c1d29458ecde745853ef146fce88e7ec4057
kevin <kevin@localhost>
2024-07-25 17:12:37 -0600

    Initial Commit

---
+++ src/main/java/com/mycompany/proyectoclienteservidor/ProyectoClienteServidor.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoclienteservidor;
+
+/**
+ *
+ * @author kevin
+ */
+public class ProyectoClienteServidor {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
